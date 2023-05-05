2d206e2c8a625c8e114912686d252a8da39b8b9d
Kristina <cheskaquinones@gmail.com>
2023-05-04 22:26:32 -0400

    update

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -6,7 +6,7 @@ export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
 export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.8");
 
 export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';
@@ -15,7 +15,7 @@ export const OPENAI_API_VERSION =
   process.env.OPENAI_API_VERSION || '2023-03-15-preview';
 
 export const OPENAI_ORGANIZATION =
-  process.env.OPENAI_ORGANIZATION || '';
+  process.env.OPENAI_ORGANIZATION || 'org-0DZtOmF4G8OfZ6SCS7FpkzTO';
 
 export const AZURE_DEPLOYMENT_ID =
   process.env.AZURE_DEPLOYMENT_ID || '';
